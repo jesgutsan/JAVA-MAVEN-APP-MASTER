123542bdc8508974d7d539eceb5d6f21b6dbfa54
jesgutsan <chusgutierrez80@gmail.com>
2025-02-05 01:40:21 +0100

    COMMIT

--- src/main/test/java/com/example/ApplicationTest.java
+++ src/main/test/java/com/example/ApplicationTest.java
@@ -1,12 +1,13 @@
 package com.example;
 
 import org.junit.Test;
-import static org.junit.Assert.assertTrue;
-
+import static org.junit.Assert.assertEquals;
 
 public class ApplicationTest {
+
     @Test
-    public void testApp() {
-        assertTrue(true); 
+    public void testGetStatus() {
+        Application app = new Application();
+        assertEquals("OK", app.getStatus());
     }
 }
