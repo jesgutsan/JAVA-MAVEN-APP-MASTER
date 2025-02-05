e2f091481dd319ec193005e7b18d707405c12d87
jesgutsan <chusgutierrez80@gmail.com>
2025-02-05 01:19:56 +0100

    commit

---
+++ src/main/test/java/com/example/AplicationTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AplicationTest {
+    @Test
+    public void testApp() {
+        assertTrue(true); // Prueba básica que siempre pasará
+    }
+}
